83cf5a21b66ccf5f296f6a1511e14d9f98650a1d
user.email <2323@163.com>
2018-01-14 19:33:55 +0800

    ejiewjj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 12343567865432
-
-# febfkjnfbehfnf
-
-
-# jfijoiwjjjjjjjjj
-
-
-#fjeffjnklewfjefnejfe;nfbeihfb
-
-
-
